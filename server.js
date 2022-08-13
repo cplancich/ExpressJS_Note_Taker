d8a85ac02d2d9a2a828e82a22d9c9355b35f0969
colinplancich <colin.plancich@gmail.com>
2022-08-12 18:49:30 -0700

    Uploading files from local machine

---
+++ server.js
@@ -0,0 +1,19 @@
+// Require express
+
+// Require the 'db.json' file and store it in 'notes'
+
+// Use express to initialize the 'app' server
+
+// Have the 'app' use appropriate middleware to parse body data
+
+// GET /nodes should return the notes.html file
+
+
+
+// GET /api/notes should read the db.json file
+
+    // res.json()
+
+// POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note
+
+// Use the 'app' to 'listen' to a specific 'port'

cb25f724aecebb67d3dbaa1cd3227548dcaffb84
colinplancich <colin.plancich@gmail.com>
2022-08-16 16:15:38 -0700

    Some changes to server.js

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 // Require express
+const express = require('express');
 
 // Require the 'db.json' file and store it in 'notes'
+const dbJson = require('./db/db.json');
 
 // Use express to initialize the 'app' server
+const app = express();
+const PORT = process.env.PORT || 3001;
 
 // Have the 'app' use appropriate middleware to parse body data
 
 // GET /nodes should return the notes.html file
-
-
 
 // GET /api/notes should read the db.json file
 
@@ -16,4 +18,7 @@
 
 // POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note
 
+// GET * should return the index.html file
+
 // Use the 'app' to 'listen' to a specific 'port'
+app.listen(PORT, () => console.log(`App listening on port ${PORT}.`));
